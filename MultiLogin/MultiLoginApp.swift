14695a2b1189634e3803f048727c64aac13a38d8
CSH <calum.haddow11@gmail.com>
2022-09-01 13:58:10 +0100

    Initial Commit

---
+++ MultiLogin/MultiLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiLoginApp.swift
+//  MultiLogin
+//
+//  Created by Calum Haddow on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
